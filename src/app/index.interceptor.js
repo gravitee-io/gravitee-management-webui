ae2d0fb071f17624f0a9e55ea88df8689bdf6d06
brasseld <brasseld@gmail.com>
2016-09-06 16:57:48 +0200

    chore: Update bower components to use ng-showdown

--- src/app/index.interceptor.js
+++ src/app/index.interceptor.js
@@ -15,15 +15,14 @@
  */
 function interceptorConfig($httpProvider) {
   'ngInject';
-//  $httpProvider.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
-//  $httpProvider.defaults.withCredentials = true;
+  $httpProvider.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
+  $httpProvider.defaults.withCredentials = true;
 
   var sessionExpired;
 
   var interceptorUnauthorized = function ($q, $injector) {
     return {
       responseError: function (error) {
-        console.log(error);
         var unauthorizedError = !error || error.status === 401;
         var errorMessage = '';
 
